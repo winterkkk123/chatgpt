bc8f7582e2f50253cfdbcdc142eb09e4adf9eff9
Baokang Liu <22030211@hdu.edu.cn>
2025-01-17 15:36:11 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # chatgpt
+
+## 项目介绍
+
+## 安装依赖
+
+npm install
+
+## 启动项目
+
+npm run dev
+
+## 项目结构
+
---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+
+</template>
+
+<script lang="ts" setup name="App">
+
+</script>
+
+<style scoped>
+  .background {
+    display: flex;
+    width: 100vw;
+    height: 100vh;
+    /* min-height: 512px;
+    min-width: 720px; */
+    /* background-color: rgb(217,234,252); */
+    justify-content: space-between;
+    align-items: flex-start;
+  }
+  
+  /* 模糊背景 */
+  /* .background-blur {
+    filter: blur(10px);
+    pointer-events: none;
+  } */
+  
+  .left-component {
+    position: relative;
+    left: 0;
+    top: 0;
+  }
+  .right-component {
+    position: relative;
+    right: 0;
+    top: 0;
+  }
+</style>
